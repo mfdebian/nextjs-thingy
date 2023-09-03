cc21d574e8adde1d32d93a4541d7c7869566c1d4
daydream <alfredo.arosteguy@gmail.com>
2023-09-02 20:37:35 -0400

    data

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
+import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+
+
+//getStaticProps only runs on the server-side. It will never run on the client-side.
+//It won’t even be included in the JS bundle for the browser. That means you can write code
+// such as direct database queries without them being sent to browsers.
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>lorem ipsum etc</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          
+          ))}
+        </ul>
       </section>
     </Layout>
   );
 }
+
